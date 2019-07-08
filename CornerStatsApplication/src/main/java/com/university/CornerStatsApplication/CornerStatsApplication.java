a9173de83a2abe69cf63e22454794b2b476be1e3
Gonzalez Tallon <agonzaleztallon@deloitte.es>
2019-07-08 19:52:40 +0200

    initial commit

---
+++ CornerStatsApplication/src/main/java/com/university/CornerStatsApplication/CornerStatsApplication.java
@@ -0,0 +1,13 @@
+package com.university.CornerStatsApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornerStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CornerStatsApplication.class, args);
+	}
+
+}
